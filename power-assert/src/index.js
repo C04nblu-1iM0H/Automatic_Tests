cd0ff48b1159e6fc96cd4238fe663aa138b99737
Nikolay <Koliansssss2014@mail.ru>
2022-08-24 14:11:29 +0300

    Проверка функции indexOf с использованием power-assert

--- power-assert/src/index.js
+++ power-assert/src/index.js
@@ -1,10 +1,15 @@
+import _ from 'lodash';
 
-
-// Весь код остаётся тем же самым
-const user = {
-  name: 'Madonna',
-  friends: ['Kate', 'Michel'],
-  email: 'madonna@example.com',
+export const user = {
+    name: 'Madonna',
+    friends: ['Kate', 'Michel'],
+    email: 'madonna@example.com',
 };
 
-export default user;
+export let indexOfLodash = (array, value, fromIndex) => {
+    return _.indexOf(array, value, fromIndex)
+}; 
+
+console.log(indexOfLodash([1, 2, 1, 2], 2));//1
+console.log(indexOfLodash([1, 2, 1, 2], 2, 2));//3
+console.log(indexOfLodash([2, 'one', 'cat', false], 8));//-1
--- power-assert/src/index.test.js
+++ power-assert/src/index.test.js
@@ -1,7 +1,13 @@
 import assert from 'power-assert';
-import user from './index';
+import {user, indexOfLodash} from './index';
+// import indexOfLodash from './index';
 
 assert(user.name === 'Madonna');
-assert.deepEqual(user.friends, ['Kate', 'Madonna']);
+assert.deepEqual(user.friends, ['Kate', 'Michel']);
+
+//проверяем на существующие элементы под данными индексами
+assert.deepEqual(indexOfLodash([1, 2, 1, 2],2), 1,); 
+assert.deepEqual(indexOfLodash([1, 2, 1, 2], 2, 2), 3);
+assert.deepEqual(indexOfLodash([2, 'one', 'cat', false], 8), -1);
 
 console.log('Все тесты пройдены!');
