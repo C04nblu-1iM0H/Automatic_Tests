6c4b87de43e88cddb1562e214dae377438afc8e3
Nikolay <Koliansssss2014@mail.ru>
2022-09-05 15:33:28 +0300

    Создание корзины и различных манипуляций над ней

---
+++ JestTests/src/cart.js
@@ -0,0 +1,28 @@
+function makeCart(){
+    return new Array(); 
+}
+
+Array.prototype.addItem = function(good, count){ //добавление товара в корзину 
+    this.push({...good, count}); //добавляем объект в массив и создаём в объекте новый ключь (count: val)
+}
+
+//выводим содержимое массива товаров, которые были добовлены в корзину
+Array.prototype.getItems = function (){
+    return this;
+}
+
+//возвращает стоимость корзины. Стоимость корзины высчитывается как сумма всех добавленных товаров с учётом их количества.
+Array.prototype.getCost = function(){
+    //1. при помощи метода map() обратился к своиствам и вычислил (количество * цену) получил общую стоимость за все одинаковые товары
+    //2. после чего этим же методом нашёл общую стоимость корзины.  
+    return this.map(c => c.price * c.count).map(c => x+=c, x=0).reverse()[0];
+}
+
+Array.prototype.getCount = function(){
+    return this.map(el => x += el.count, x=0).reverse()[0];
+}
+
+Array.prototype.remove = function(elName){
+    let index = this.map((item) => item.name).indexOf(elName);
+    return this.splice(index, 1);
+}
